ecef16bdef80e5b0796686b4118880d7fdd9e811
Jason Staten <jason.staten@domo.com>
2018-08-04 23:11:54 -0600

    parsing all the tokens

--- src/ch09_compiler/lexer.rs
+++ src/ch09_compiler/lexer.rs
@@ -4,12 +4,13 @@ use std::iter::Peekable;
 use std::str::CharIndices;
 
 #[derive(Debug, PartialEq)]
-enum Token {
+enum Token<'a> {
     EOF,
     Def,
     Extern,
-    Identifier(String),
+    Identifier(&'a str),
     Number(f64),
+    Illegal,
 }
 
 struct Lexer<'a> {
@@ -25,67 +26,51 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn read_identifier(&mut self, index: usize) -> Option<Token> {
+    fn read_identifier(&mut self, index: usize) -> Token<'a> {
         let rest = &self.buf[index..];
         match rest.split_whitespace().next() {
-            Some("def") => Some(Token::Def),
-            _ => None,
+            Some("def") => {
+                self.buf_iter.nth(2);
+                Token::Def
+            }
+            Some("extern") => {
+                self.buf_iter.nth(5);
+                Token::Extern
+            }
+            Some(word) => {
+                self.buf_iter.nth(word.len() - 1);
+                Token::Identifier(word)
+            }
+            None => Token::Illegal,
         }
+    }
+
+    fn read_number(&mut self, index: usize) -> Token<'a> {
+        let rest = &self.buf[index..];
+        let word = rest.split_whitespace().next();
+
+        word.and_then(|w| w.parse::<f64>().ok())
+            .map_or(Token::Illegal, Token::Number)
     }
 }
 
 impl<'a> Iterator for Lexer<'a> {
-    type Item = Token;
+    type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut iter = self.buf_iter.clone();
-
-        match iter.peek() {
-            Some((i, x)) if x.is_alphabetic() => self.read_identifier(i.clone()),
+        while self
+            .buf_iter
+            .peek()
+            .map_or(false, |&(_, x)| x.is_whitespace())
+        {
+            self.buf_iter.next();
+        }
+        match self.buf_iter.peek() {
+            Some(&(i, x)) if x.is_alphabetic() => Some(self.read_identifier(i)),
+            Some(&(i, x)) if x.is_numeric() => Some(self.read_number(i)),
             _ => None,
         }
     }
-}
-
-fn get_token<R: Read>(input: &mut R) -> Token {
-    let mut chars = input.bytes().map(|b| char::from(b.unwrap()));
-
-    let last_result = chars.next();
-    if last_result.is_none() {
-        return Token::EOF;
-    }
-
-    let last_char = last_result.unwrap();
-
-    if last_char.is_alphabetic() {
-        let mut id = String::new();
-        id.push(last_char);
-
-        let tail = chars
-            .take_while(|c| c.is_alphanumeric())
-            .collect::<String>();
-        id.push_str(&tail);
-
-        return match id.as_ref() {
-            "def" => Token::Def,
-            "extern" => Token::Extern,
-            _ => Token::Identifier(id),
-        };
-    }
-
-    if last_char.is_numeric() {
-        let mut num = String::new();
-        num.push(last_char);
-
-        let tail = chars
-            .take_while(|c| c.is_numeric() || c == &'.')
-            .collect::<String>();
-        num.push_str(&tail);
-
-        return Token::Number(num.parse::<f64>().unwrap());
-    }
-
-    Token::EOF
 }
 
 #[cfg(test)]
@@ -101,6 +86,32 @@ mod test {
     fn test_it_parses_def() {
         let mut lex = Lexer::new("def");
         assert_eq!(lex.next(), Some(Token::Def));
+    }
+
+    #[test]
+    fn test_it_parses_extern() {
+        let mut lex = Lexer::new("extern");
+        assert_eq!(lex.next(), Some(Token::Extern));
+    }
+
+    #[test]
+    fn test_it_parses_identifier() {
+        let mut lex = Lexer::new("rock");
+        assert_eq!(lex.next(), Some(Token::Identifier("rock")));
+    }
+
+    #[test]
+    fn test_it_parses_number() {
+        let mut lex = Lexer::new("42.7");
+        assert_eq!(lex.next(), Some(Token::Number(42.7f64)));
+    }
+
+    #[test]
+    fn test_it_parses_multiple() {
+        let mut lex = Lexer::new("def hello_world 42");
+        assert_eq!(lex.next(), Some(Token::Def));
+        assert_eq!(lex.next(), Some(Token::Identifier("hello_world")));
+        assert_eq!(lex.next(), Some(Token::Number(42f64)));
     }
 
 }
